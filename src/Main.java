e923c61297acc5753af2548f95ccb6825dfdd692
Yutta <111496203+Yutta-u@users.noreply.github.com>
2022-08-20 14:56:14 +0500

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,27 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        int a = 5;
 
-        if ((a < 2) || (a > 3)) {
-            System.out.println("yes");
-        } else {
-            System.out.println("no");
+        System.out.println("enter your rating for the tests ");
+
+    Scanner scan = new Scanner(System.in);
+
+        int number = scan.nextInt();
+
+        switch (number) {
+            case 2:
+                System.out.println("its f*cking bad man");
+                break;
+            case 3:
+                System.out.println("still bad");
+                break;
+            case 4:
+                System.out.println("already good");
+                break;
+            case 5:
+                System.out.println("good boy");
+                break;
         }
-        System.out.println(" Good bye!");
+        System.out.println("Bye Bye!!");
     }
 }
